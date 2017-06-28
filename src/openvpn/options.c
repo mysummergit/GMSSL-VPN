4a4e34b9adf5b29676cb50bad1cf4e8e4f6a0593
summer <1460246652@qq.com>
2017-06-28 16:32:07 +0800

    OPENVPN check cert

--- src/openvpn/options.c
+++ src/openvpn/options.c
@@ -3249,6 +3249,7 @@ options_postprocess_filechecks(struct options *options)
     errs |= check_file_access(CHKACC_FILE|CHKACC_INLINE, options->dh_file, R_OK, "--dh");
     errs |= check_file_access(CHKACC_FILE|CHKACC_INLINE, options->ca_file, R_OK, "--ca");
     errs |= check_file_access_chroot(options->chroot_dir, CHKACC_FILE, options->ca_path, R_OK, "--capath");
+	printf("check_file_access cert_file\n");
     errs |= check_file_access(CHKACC_FILE|CHKACC_INLINE, options->cert_file, R_OK, "--cert");
     errs |= check_file_access(CHKACC_FILE|CHKACC_INLINE, options->extra_certs_file, R_OK,
                               "--extra-certs");
